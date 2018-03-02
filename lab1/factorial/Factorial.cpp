f99a67b13a351a3e589887c156f81f1703fca52e
smiezuza <smiezuza@agh.edu.pl>
2018-03-02 16:09:05 +0100

    Solution to reverse string

--- lab1/factorial/Factorial.cpp
+++ lab1/factorial/Factorial.cpp
@@ -4,5 +4,14 @@
 #include "Factorial.h"
 
 int factorial(int value) {
-  return 0;
+  if(value>=13 || value<=-13) {
+        return 0;
+  }
+  if (value == 0){
+      return 1;
+  }
+  if (value>0) {
+      return value * factorial(value - 1);
+  }
+  return value *factorial(value+1);
 }
