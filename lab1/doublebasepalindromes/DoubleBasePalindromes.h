03d208ad25b5ad52850a4dd8174b0fc5dd286b76
szuzanna <zdrapka111@gmail.com>
2018-03-04 20:43:03 +0100

    Adding new feature - double base palindromes verification. Unit tests - passed.

---
+++ lab1/doublebasepalindromes/DoubleBasePalindromes.h
@@ -0,0 +1,16 @@
+//
+// Created by Zuzanna on 04.03.2018.
+//
+
+#ifndef JIMP_EXERCISES_DOUBLEBASEPALINDROMES_H
+#define JIMP_EXERCISES_DOUBLEBASEPALINDROMES_H
+
+
+#include <cstdint>
+#include <string>
+
+uint64_t DoubleBasePalindromes(int max_vaule_exculsive);
+std::string DecToBin(int number);
+bool isPalindrome(std::string str);
+
+#endif //JIMP_EXERCISES_DOUBLEBASEPALINDROMES_H
---
+++ lab1/doublebasepalindromes/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Zuzanna on 04.03.2018.
+//
+#include <iostream>
+#include "DoubleBasePalindromes.h"
+
+int main(){
+    std::cout << DoubleBasePalindromes(300'000) << std::endl; 
+}
