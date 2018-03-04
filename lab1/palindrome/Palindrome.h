e2fca2530e0c9151d728abd98888e74a41104eca
szuzanna <zdrapka111@gmail.com>
2018-03-04 12:38:40 +0100

    Adding new feature - palindrome verification. Unit test - passed.

---
+++ lab1/palindrome/Palindrome.h
@@ -0,0 +1,11 @@
+//
+// Created by Zuzanna on 02.03.2018.
+//
+#include <string>
+#ifndef JIMP_EXERCISES_PALINDROME_H
+#define JIMP_EXERCISES_PALINDROME_H
+
+
+bool is_palindrome(std::string str);
+
+#endif //JIMP_EXERCISES_PALINDROME_H
